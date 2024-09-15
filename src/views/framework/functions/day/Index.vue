a56a65db5c380d069ed79ccc5c29682975400f14
LauZzL <lauzzl@163.com>
2024-09-15 09:18:17 +0800

    防止溢出

--- src/views/framework/functions/day/Index.vue
+++ src/views/framework/functions/day/Index.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
-        <el-space>
-            <el-button @click="sendCmd({ cmd:201 })">每日广告体力×1次</el-button>
-            <el-button @click="sendCmd({ cmd:206 })">每日广告体力×4次</el-button>
-            <el-button @click="sendCmd({ cmd:202 })">商城广告奖励</el-button>
-            <el-button @click="sendCmd({ cmd:203 })">寻宝装备宝箱</el-button>
-            <el-button @click="sendCmd({ cmd:204 })">星际探索</el-button>
-            <el-button @click="sendCmd({ cmd:205 })">幸运转盘</el-button>
-        </el-space>
-    </div>
+  <div>
+    <el-space wrap>
+      <el-button @click="sendCmd({ cmd:201 })">每日广告体力×1次</el-button>
+      <el-button @click="sendCmd({ cmd:206 })">每日广告体力×4次</el-button>
+      <el-button @click="sendCmd({ cmd:202 })">商城广告奖励</el-button>
+      <el-button @click="sendCmd({ cmd:203 })">寻宝装备宝箱</el-button>
+      <el-button @click="sendCmd({ cmd:204 })">星际探索</el-button>
+      <el-button @click="sendCmd({ cmd:205 })">幸运转盘</el-button>
+    </el-space>
+  </div>
 </template>
 
 <script setup>
