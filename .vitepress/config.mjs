033db593efb1a9a10bfd353776095e590504bdef
LauZzL <lauzzl@163.com>
2024-09-26 15:48:53 +0800

    update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "leitingzhanji-docs",
   title: "LTZJ-NEM",
   description: "LTZJ-NEM Documentation",
   lastUpdated: true,
